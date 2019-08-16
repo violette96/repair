d4cb80531584b492b24cefa7d392df574cca5a94
Анастасия <violette0696@gmail.com>
2019-08-16 15:12:15 +0300

    добавлен блок со слайдерами, которые подключены с помощью библиотеки slick

--- js/modal.js
+++ js/modal.js
@@ -2,11 +2,19 @@ $(document).ready(function(){
  var button = $('#button');
  var modal = $('#modal');
  var close = $('#close');
+ var cardLink = $('.card__link')
  
  button.on('click', function(){
   modal.addClass('modal_active');
  });
 
+ /* cardLink.on('click', function(event){
+  event.preventDefault()// это функция, которая отключает станадартное поведение ссылки
+  modal.addClass('modal_active');
+  var target = $(this).attr('data-target');
+  $('.modal-content').load(target+.html);
+ });
+ */
  close.on('click', function(){
   modal.removeClass('modal_active');
 
